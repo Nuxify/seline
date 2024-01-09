aab00967ae87211c6cb5cfa5d2886a1026ece3f9
danvincent1122 <danvincent1122@gmail.com>
2024-01-09 16:15:22 +0800

    fix: add alert

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-	import { Button } from 'flowbite-svelte';
+	import { Alert, Button } from 'flowbite-svelte';
 </script>
 
 <h1 class="text-primary text-xl">Svelte Template: Seline</h1>
 
 <Button size="sm" color="red" class="mt-3 px-5">Submit</Button>
+<Alert
+	dismissable
+	class="fixed bottom-0 left-1/2 w-[99%] -translate-x-1/2 mb-2 bg-primary text-white"
+>
+	Submitted content successfully!
+</Alert>
