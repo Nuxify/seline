a369fc24e02e3c1ce6caf8901d483840df84eca7
danvincent1122 <danvincent1122@gmail.com>
2024-01-20 18:39:13 +0800

    feat: fetch post method

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,34 +3,83 @@
 	import { SEO } from '$components';
 
 	export let data;
+	let title: string;
+	let body: string;
+	let isLoading: boolean = false;
+	let isShowAlert: boolean = false;
+
+	/**
+	 * Create a post
+	 */
+	async function createPost(): Promise<void> {
+		isLoading = true;
+		try {
+			const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+				method: 'POST',
+				body: JSON.stringify({ title, body }),
+				headers: { 'Content-Type': 'application/json' }
+			});
+			if (response.ok) {
+				const responseData = await response.json();
+
+				data.posts.unshift(responseData);
+				data.posts = data.posts.slice();
+
+				title = '';
+				body = '';
+				// Alert prompt
+				isShowAlert = true;
+				setTimeout(() => {
+					isShowAlert = false;
+				}, 3000);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+		isLoading = false;
+	}
 </script>
 
-<!-- <Button size="sm" color="red" class="mt-3 px-5">Submit</Button> -->
-<!-- <Alert
-	dismissable
-	class="fixed bottom-0 left-1/2 mb-2 w-[99%] -translate-x-1/2 bg-primary text-white"
->
-	Submitted content successfully!
-</Alert> -->
+{#if isShowAlert}
+	<Alert
+		dismissable
+		class="fixed left-1/2 top-0 w-full -translate-x-1/2 rounded-none bg-primary text-white"
+	>
+		Submitted content successfully!
+	</Alert>
+{/if}
 
 <section>
 	<div class="mx-auto mt-[30px] px-4 xl:container">
 		<h1 class="text-center text-2xl text-primary">Seline: Social Media</h1>
-		<div class="w-3/4">
+		<div class="mx-auto mb-7 mt-12 w-3/4">
 			<h1 class="text-xl text-primary">Create a post</h1>
 			<div class="flex flex-col items-center md:flex-row">
 				<Input
-					type="email"
-					placeholder="Enter comment"
-					class="md:border--radius-l-full w-full rounded-full border-none bg-gray-100 !ring-transparent md:w-64"
+					bind:value={title}
+					type="text"
+					placeholder="Enter title"
+					class="md:border--radius-l-full mr-5 w-full w-full rounded-full border-none bg-gray-100 !ring-transparent"
 				/>
-				<Button class="btn--primary mt-3 w-full rounded-full md:ml-[-30px] md:mt-0 md:w-auto">
-					<span class="lato--font-regular text-white"> Post </span>
+				<Input
+					bind:value={body}
+					type="text"
+					placeholder="Enter body"
+					class="md:border--radius-l-full w-full w-full rounded-full border-none bg-gray-100 !ring-transparent"
+				/>
+				<Button
+					class="btn--primary ml-5 w-[100px] rounded-full"
+					disabled={isLoading}
+					on:click={() => {
+						createPost();
+					}}
+				>
+					<span class="lato--font-regular px-4 text-white"> Post </span>
 				</Button>
 			</div>
 		</div>
 		{#each data.posts as item}
-			<div class="mb-7rounded-xl mx-auto w-3/4 border border-primary p-5 rounded-lg my-2">
+			<div class="mb-7rounded-xl mx-auto my-2 w-3/4 rounded-lg border border-primary p-5">
 				<h6>ID: {item.id}</h6>
 				<h5 class="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">
 					{item.title}
