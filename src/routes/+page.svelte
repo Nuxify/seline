c07ae3ced0e7d1ef7be783741b3dbffffb3951f7
danvincent1122 <danvincent1122@gmail.com>
2024-01-20 17:48:41 +0800

    fix: update alert size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <Button size="sm" color="red" class="mt-3 px-5">Submit</Button>
 <Alert
 	dismissable
-	class="fixed left-1/2 top-0 w-full -translate-x-1/2 rounded-none bg-primary text-white"
+	class="fixed left-1/2 top-0 w-full -translate-x-1/2 rounded-none bg-primary py-3 text-white"
 >
 	<span class="font-semibold">Submitted content successfully!</span>
 </Alert>
