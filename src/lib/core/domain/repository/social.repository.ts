5e590b4ff9533ef1eeefd993e8e87cccf3de97ef
danvincent1122 <danvincent1122@gmail.com>
2024-10-31 16:01:45 +0800

    docs: add comments for interface methods

--- src/lib/core/domain/repository/social.repository.ts
+++ src/lib/core/domain/repository/social.repository.ts
@@ -7,7 +7,28 @@ import type {
 } from '$lib/core/domain/models/social.dto'
 
 export interface ISocialRepository {
+	/**
+	 * Create a post
+	 *
+	 * @param   {CreatePostRequest<APIResponse><CreatePostResponse>}  request
+	 *
+	 * @return  {Promise<APIResponse><CreatePostResponse>}
+	 */
 	CreatePost(request: CreatePostRequest): Promise<APIResponse<CreatePostResponse>>
-	GetAllPosts(): Promise<APIResponse<PostResponse[]>>
+
+	/**
+	 * Get posts
+	 *
+	 * @return  {Promise<APIResponse><PostResponse>[]}
+	 */
+	GetPosts(): Promise<APIResponse<PostResponse[]>>
+
+	/**
+	 * Get post comments
+	 *
+	 * @param   {number<APIResponse><CommentResponse>[]}   postId
+	 *
+	 * @return  {Promise<APIResponse><CommentResponse>[]}
+	 */
 	GetPostComments(postId: number): Promise<APIResponse<CommentResponse[]>>
 }
