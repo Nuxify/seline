ad6ce10a1d1616f04c66b6be4996e08c4a24ed91
danvincent1122 <danvincent1122@gmail.com>
2024-10-31 15:35:33 +0800

    fix: revert previous modifications in servicecontainer

--- src/lib/servicecontainer.ts
+++ src/lib/servicecontainer.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import { AxiosConfig } from '$lib/configs/axios'
-import type { ISocialRepository as SocialRepositoryInterface } from '$lib/core/domain/repository/social.repository'
-import { SocialRepository } from '$lib/core/infrastructures/repository/social.repository'
-import type { ILocalStorageRepository as LocalStorageRepositoryInterface } from '$lib/core/domain/repository/localstorage.repository'
-import { LocalStorageRepository } from '$lib/core/infrastructures/repository/localstorage.repository'
+import type { ISocialRepository } from '$lib/core/domain/repository/social.repository'
+import { SocialRepository as SocialRepositoryImpl } from '$lib/core/infrastructures/repository/social.repository'
+import type { ILocalStorageRepository } from '$lib/core/domain/repository/localstorage.repository'
+import { LocalStorageRepository as LocalStorageRepositoryImpl } from '$lib/core/infrastructures/repository/localstorage.repository'
 
 /// dependency injection
 const $axios = axios.create(AxiosConfig) // base axios
 
-export const ISocialRepository: SocialRepositoryInterface = new SocialRepository($axios)
-export const ILocalStorageRepository: LocalStorageRepositoryInterface = new LocalStorageRepository()
+export const SocialRepository: ISocialRepository = new SocialRepositoryImpl($axios)
+export const LocalStorageRepository: ILocalStorageRepository = new LocalStorageRepositoryImpl()
