88d627d31b840dff5494f6d45586e7b2dcafff63
anya <txcokun@gmail.com>
2026-03-30 18:23:52 +0800

    wip(tests): add projects, url and trace on playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import type { PlaywrightTestConfig } from '@playwright/test'
+import { devices, type PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
 	webServer: {
@@ -6,7 +6,57 @@ const config: PlaywrightTestConfig = {
 		port: 4173
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	fullyParallel: true,
+	reporter: 'html',
+	use: {
+		// Base URL to use in actions like `await page.goto('/')`.
+		baseURL: 'http://localhost:3000',
+		// Collect trace when retrying the failed test.
+		trace: 'on-first-retry'
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
+		}
+
+		// /* Test against mobile viewports. */
+		// {
+		// 	name: 'Mobile Chrome',
+		// 	use: { ...devices['Pixel 5'] }
+		// },
+		// {
+		// 	name: 'Mobile Safari',
+		// 	use: { ...devices['iPhone 12'] }
+		// },
+
+		// /* Test against branded browsers. */
+		// {
+		// 	name: 'Microsoft Edge',
+		// 	use: {
+		// 		...devices['Desktop Edge'],
+		// 		channel: 'msedge'
+		// 	}
+		// },
+		// {
+		// 	name: 'Google Chrome',
+		// 	use: {
+		// 		...devices['Desktop Chrome'],
+		// 		channel: 'chrome'
+		// 	}
+		// }
+	]
 }
 
 export default config
