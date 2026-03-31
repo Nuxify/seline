1921647ac1796bd81f92883ec109bb237330168d
anya <txcokun@gmail.com>
2026-03-31 10:22:54 +0800

    fix(tests): add reuse server, update baseURL on config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,16 +3,15 @@ import { devices, type PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		reuseExistingServer: true
 	},
-	testDir: 'tests',
+	testDir: 'tests/e2e',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	fullyParallel: true,
 	reporter: 'html',
 	use: {
-		// Base URL to use in actions like `await page.goto('/')`.
-		baseURL: 'http://localhost:3000',
-		// Collect trace when retrying the failed test.
+		baseURL: 'http://localhost:5173/',
 		trace: 'on-first-retry'
 	},
 	projects: [
@@ -29,33 +28,16 @@ const config: PlaywrightTestConfig = {
 		{
 			name: 'webkit',
 			use: { ...devices['Desktop Safari'] }
-		}
-
+		},
 		// /* Test against mobile viewports. */
-		// {
-		// 	name: 'Mobile Chrome',
-		// 	use: { ...devices['Pixel 5'] }
-		// },
-		// {
-		// 	name: 'Mobile Safari',
-		// 	use: { ...devices['iPhone 12'] }
-		// },
-
-		// /* Test against branded browsers. */
-		// {
-		// 	name: 'Microsoft Edge',
-		// 	use: {
-		// 		...devices['Desktop Edge'],
-		// 		channel: 'msedge'
-		// 	}
-		// },
-		// {
-		// 	name: 'Google Chrome',
-		// 	use: {
-		// 		...devices['Desktop Chrome'],
-		// 		channel: 'chrome'
-		// 	}
-		// }
+		{
+			name: 'Mobile Chrome',
+			use: { ...devices['Pixel 5'] }
+		},
+		{
+			name: 'Mobile Safari',
+			use: { ...devices['iPhone 12'] }
+		}
 	]
 }
 
