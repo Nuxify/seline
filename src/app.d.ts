6ee3a14c8646311e0e8590897500c22ca6346488
anya <txcokun@gmail.com>
2026-04-07 15:35:42 +0800

    refactor(core): remove typescript svelte declaration module

--- src/app.d.ts
+++ src/app.d.ts
@@ -10,16 +10,4 @@ declare global {
 	}
 }
 
-// Svelte module declarations for TypeScript
-// Allows importing `.svelte` components without implicit any.
-declare module '*.svelte' {
-	import { SvelteComponentTyped } from 'svelte'
-
-	export default class Component<
-		Props = any,
-		Events = Record<string, any>,
-		Slots = Record<string, any>
-	> extends SvelteComponentTyped<Props, Events, Slots> {}
-}
-
 export {}
