e25245ac096f5fc36a1cc712ff0b06cfe32e7d71
anya <txcokun@gmail.com>
2026-04-08 15:05:13 +0800

    fix(tests): change comment component and tests to use test api data

--- src/lib/core/interfaces/components/Comment.svelte
+++ src/lib/core/interfaces/components/Comment.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
-  let {name} = $props()
-
-  let showGreeting:boolean = $state(false)
-
-  const onclick = () => (showGreeting = true)
+    let {name, email, body} = $props()
 </script>
 
-<button {onclick}>
-    {#if showGreeting}
-<p>Hello {name}</p>
-{:else}
-    Greet
-{/if}</button>
+<div class="mb-7rounded-xl mx-auto my-2 w-3/4 rounded-lg border border-primary p-5">
+    <h6>Name: {name}</h6>
+    <h5 class="mb-2 text-sm font-medium lowercase tracking-tight text-gray-500 dark:text-white">
+        {email}
+    </h5>
+    <p class="font-normal leading-tight text-gray-700 dark:text-gray-400">{body}</p>
+</div>
 
--- tests/component/comment.svelte.test.ts
+++ tests/component/comment.svelte.test.ts
@@ -5,11 +5,14 @@ import Comment from '$lib/core/interfaces/components/Comment.svelte'
 
 test('Comment', async () => {
 	const user = userEvent.setup()
-	render(Comment, { name: 'Seline' })
+	render(Comment, { name: 'Seline', email: 'seline@test.com', body: 'This is a comment' })
 
-	const button = screen.getByRole('button')
-	expect(button.textContent).toContain('Greet')
+	const name = screen.getByRole('heading', { level: 6 })
+	expect(name.textContent).toContain('Seline')
 
-	await user.click(button)
-	expect(button.textContent).toContain('Hello Seline')
+	const email = screen.getByRole('heading', { level: 5 })
+	expect(email.textContent).toContain('seline@test.com')
+
+	const body = screen.getByRole('paragraph')
+	expect(body.textContent).toEqual('This is a comment')
 })
